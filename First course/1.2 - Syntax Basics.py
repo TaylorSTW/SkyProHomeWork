558513f151142ddcf847aa33f07037d2813a28f9
Dmitry <108948093+TaylorSTW@users.noreply.github.com>
2022-12-10 21:45:25 +0300

    Redacting code

--- First course/1.2 - Syntax Basics.py
+++ First course/1.2 - Syntax Basics.py
@@ -1,9 +1,11 @@
 points = 0
 count = 0
 procent = 100
+
 print("Привет! Предлагаю проверить свои знания английского!")
 name = input("Расскажи, как тебя зовут! ")
 print("Привет, " + name + " ,начинаем тренировку!")
+
 q1 = input("My name ___ Vova  ")
 q1 = q1.lower()
 if q1 == "is":
@@ -13,6 +15,7 @@ if q1 == "is":
 else:
   print("Неправильно.\nПравильный ответ = is")
   procent = round(procent - 100 / 3, 2)
+
 q2 = input("I ___ a coder  ")
 q2 = q2.lower()
 if q2 == "am":
@@ -22,6 +25,7 @@ if q2 == "am":
 else:
   print("Неправильно.\nПравильный ответ = am")
   procent = round(procent - 100 / 3, 2)
+
 q3 = input("I live ___ Moscow  ")
 q3 = q3.lower()
 if q3 == "in":
